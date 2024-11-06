f7495e148cd1b677ae40df7e2e5c5349db1e6ed2
BKocel <bartosz.kocel@outlook.com>
2024-11-06 13:17:30 +0100

    modified:   Test.py

--- Test.py
+++ Test.py
@@ -4,3 +4,11 @@ suma = 0
 for i in range(1,n+1):
    suma += i 
 print(suma)
+
+
+def suma_rekurencyja(n):
+   if n==0:
+      return 0
+   return n+suma_rekurencyja(n-1)
+
+print(suma_rekurencyja(n))
